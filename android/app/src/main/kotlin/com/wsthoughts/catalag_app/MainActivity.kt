ffa61bed2a557a31d55c5597a0c803c95cb88a00
Waqas <waqasakhtar@gmail.com>
2024-03-06 15:16:51 +0500

    Lecture 1

---
+++ android/app/src/main/kotlin/com/wsthoughts/catalag_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.wsthoughts.catalag_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Catalog App",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(child: Text("Welcome to My Catalog App")),
+      drawer: const Drawer(),
+    );
+  }
+}
